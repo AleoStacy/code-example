a41c02f56221ede81fe4dd24fb5f38884d95d180
Stacy Aleyo <aleyostacy@gmail.com>
2026-04-28 13:18:44 +0300

    backend ready for deployment

--- backend/app.py
+++ backend/app.py
@@ -12,5 +12,8 @@ def home():
 def data():
     return jsonify({"message": "So much 🔥"})
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
